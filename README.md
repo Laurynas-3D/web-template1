a50d68a0cd94ead2bb6cf39f5e9c93966f8d0ab2
Laurynas-3D <lauriz99@gmail.com>
2020-07-29 19:32:47 +0300

    20200729

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # web-template1
-Vue.js playground
+Vue.js framework playground
 
 v1
+-
 ![screen1](https://user-images.githubusercontent.com/46622469/88823059-53a45180-d1cd-11ea-94ea-87e44afbcb92.png)
--- templateZero.js
+++ templateZero.js
@@ -38,9 +38,9 @@ Vue.component('product', {
         onSale: true,
         selectedVariant: 0,
         details: ["Ceramic coffee mug", "325mL", "microwave and oven safe"],
-        variants: [{variantId: 1,variantColor: "Yellow", variantImage:"./img/cup-yellow.png", variantQuantity:10},
-                   {variantId: 2,variantColor: "Green", variantImage:"./img/cup-green.png", variantQuantity:2},
-                   {variantId: 3,variantColor: "Red", variantImage:"./img/cup-red.png", variantQuantity:0}],
+        variants: [{variantId: 1,variantColor: "Yellow", variantImage:"./img/graphics/cup-yellow.png", variantQuantity:10},
+                   {variantId: 2,variantColor: "Green", variantImage:"./img/graphics/cup-green.png", variantQuantity:2},
+                   {variantId: 3,variantColor: "Red", variantImage:"./img/graphics/cup-red.png", variantQuantity:0}],
         cart: 0
 
     }  // end data return
